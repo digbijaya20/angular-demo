db94f56fe5995cfaef24d24814d869ab501257a3
digbijaya20 <digbijaya.biswal1234@gmail.com>
2022-06-04 23:31:50 +0530

    updated recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Chicken","Agood iem for digention sysem , if u know u know",
+    "https://bigoven-res.cloudinary.com/image/upload/sweetandsourstickythaiboneless-3a944d.jpg")
+  ];
+
 
   constructor() { }
 
